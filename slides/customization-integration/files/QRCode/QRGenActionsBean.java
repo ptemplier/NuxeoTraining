9c9f628df64b7b0c07210b53e05501d55d975fd9
bchauvin <bchauvin@nuxeo.com>
2014-05-14 15:22:37 +0200

    moved files for better clarity, finalized slides on building an extension point

---
+++ slides/customization-integration/files/QRCode/QRGenActionsBean.java
@@ -0,0 +1,62 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Bertrand Chauvin
+ */
+
+package org.nuxeo.sample;
+
+import java.io.Serializable;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.faces.FacesMessages;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
+import org.nuxeo.ecm.webapp.documentsLists.DocumentsListsManager;
+import org.nuxeo.ecm.webapp.helpers.ResourcesAccessor;
+import org.nuxeo.runtime.api.Framework;
+
+@Name("qrGenTest")
+@Scope(ScopeType.EVENT)
+public class QRGenActionsBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @SuppressWarnings("unused")
+    private static final Log log = LogFactory.getLog(QRGenActionsBean.class);
+
+    @In(create = true)
+    protected NavigationContext navigationContext;
+
+    // This the method that will be called when the action button is clicked
+    public String generateQR() throws ClientException {
+        BestBooksService bb = Framework.getLocalService(BestBooksService.class);
+        bb.addQR(navigationContext.getCurrentDocument(), "book:qrcode", "titleVersion");
+
+        // stay on the same view
+        return null;
+    }
+
+    public boolean accept() {
+        return true;
+    }
+
+}
