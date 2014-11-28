8d7c08daff67cf08574e5e31699faeb01ded1ab8
Bertrand Chauvin <bchauvin@nuxeo.com>
2014-11-28 12:45:11 +0100

    Added js client workshop

---
+++ nuxeo-jsclient-workshop/src/main/resources/web/nuxeo.war/restapi.js
@@ -0,0 +1,349 @@
+var RestAPI = {};
+
+////////////////////////////// NUXEO JS CONFIGURATION
+
+// Configure Nuxeo Client
+RestAPI.config = function () {
+  //Instantiate Nuxeo Client below
+  var client = new nuxeo.Client({
+  baseURL: 'http://localhost:8080/nuxeo',
+  username: 'Administrator',
+  password: 'Administrator'
+  });
+
+  // Make sure:
+  // - That the client will return the dublincore and file schemas
+  // - To return the client afterwards
+  client.schema('dublincore,file');
+  return client;
+}
+
+////////////////////////////// CURRENT USER
+
+// Use a GET request to get the user: Administrator
+// Callback: callbackCurrentUser
+RestAPI.getCurrentUser = function () {
+  this.client.request('user/Administrator').get(callbackCurrentUser);
+}
+
+////////////////////////////// EXECUTE QUERY
+
+// Use a GET request to execute the query typed 
+// Make sure to include the query parameter in the called URL
+// Callback: callbackQuery
+RestAPI.executeQuery = function (query) {
+  this.client.request('query/?query=' + query).get(callbackQuery);
+}
+
+////////////////////////////// DISPLAY WORKSPACE CHILDREN
+
+// Use the high level document API to get the "documentRoot" children
+// Callback: callbackRootChildren
+var documentRoot = "/default-domain/workspaces/IT";
+RestAPI.getRootChildren = function () {
+  this.client.document(documentRoot).children(callbackRootChildren);
+}
+
+
+////////////////////////////// DISPLAY DOCUMENT PROPERTIES
+
+// Use the high level document API to get a document using its id 
+// Callback: callbackFetchDocument
+RestAPI.fetchDocument = function (id) {
+  this.client.document(id).fetch(callbackFetchDocument);
+  //this.client.document(id).schemas(["common","dublincore"]).fetch(callbackFetchDocument);
+  //this.client.document(id).header('X-NXContext-Category', 'acls').fetch(callbackFetchDocument);
+}
+
+////////////////////////////// UPDATE DOCUMENT
+
+// Use the high level document API to update the current document
+// And save it afterwards
+// Callback: callbackUpdateDocument
+RestAPI.updateDocument = function (map) {
+  this.currentDocument.set(map).save(callbackUpdateDocument)
+}
+
+////////////////////////////// CREATE DOCUMENT
+
+// Use the high level document API to create a document
+// Form data can be accessed through map["schema-prefix:metadata"]
+// e.g.: map["dc:title"]
+// Callback: callbackCreateDocument
+var creationRoot = "/default-domain/workspaces/IT";
+RestAPI.createDocument = function (map) {
+  this.client.document(creationRoot)
+    .create({
+      type: 'File',
+      name: map["dc:title"],
+      properties: {
+        "dc:title": map["dc:title"],
+        "dc:description": map["dc:description"],
+        "dc:nature": map["dc:nature"],
+        "dc:language": map["dc:language"]
+      }
+    }, callbackCreateDocument);
+}
+
+////////////////////////////// DELETE DOCUMENT
+
+// Use the high level document API to delete the current document
+// Callback: callbackDeleteDocument
+RestAPI.deleteDocument = function () {
+  this.currentDocument.delete(callbackDeleteDocument);
+}
+
+////////////////////////////// FILE IMPORT
+
+// Use the uploader API to import a file
+// Operation to call: FileManager.Import (Create document from file)
+// Callback: callbackImportFile
+var importRoot = "/default-domain/workspaces/IT";
+RestAPI.importFile = function (file) {
+  var uploader = this.client.operation("FileManager.Import").context({ currentDocument: importRoot }).uploader();
+  uploader.uploadFile(file, function () {
+    uploader.execute(callbackImportFile);
+  });
+}
+
+////////////////////////////// ATTACH BLOB
+
+// Use the uploader API to attach a file in an existing document
+// File is to be saved into the "file:content" xpath
+// Operation to call: Blob.Attach
+// Callback: callbackAttachBlob
+RestAPI.attachBlob = function (file) {
+  var uploader = this.client.operation("Blob.Attach")
+    .params({ document: this.currentDocument.uid, save: true,
+      xpath: "file:content"}).uploader();
+  uploader.uploadFile(file, function () {
+    uploader.execute(callbackAttachBlob)
+  });
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// DO NOT EDIT THE CODE BELOW //////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+function callbackCreateDocument(error, file) {
+  if (error) {
+    throwError("Cannot create document -> " + error);
+    throw error;
+  }
+  console.log('Created ' + file.title + ' file')
+  location.reload();
+}
+
+function importFiles(files) {
+  for (var i = 0; i < files.length; i++) {
+    RestAPI.importFile(files[i]);
+  }
+}
+
+function callbackDeleteDocument(error, data) {
+  $('#modalDelete').modal('hide');
+  if (error) {
+    throwError("Cannot delete document -> " + error);
+    throw error;
+  }
+  location.reload();
+  console.log("Document has been deleted");
+}
+
+function callbackAttachBlob(error, data) {
+  $('#modalBlob').modal('hide');
+  if (error) {
+    throwError("Cannot attach blob -> " + error);
+    throw error;
+  }
+  RestAPI.fetchDocument(RestAPI.currentDocument.uid);
+  console.log("Blob has been attached");
+}
+
+function callbackImportFile(error, data) {
+  if (error) {
+    throwError("Cannot import files -> " + error);
+    throw error;
+  }
+  location.reload();
+  console.log("Files has been imported");
+}
+
+function save() {
+  var map = new Object();
+  $("#docForm :input").each(function () {
+    var name = $(this).attr('name');
+    var input = $(this).val();
+    map[name] = input;
+  });
+  RestAPI.updateDocument(map);
+}
+
+function callbackCurrentUser(error, user) {
+  if (error) {
+    throwError("Cannot get user -> " + error);
+    throw error;
+  }
+  console.log(user);
+  var template = $('#userId').html();
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {userId: user.id});
+  $('#targetUserId').html(rendered);
+  template = $('#userInfos').html();
+  Mustache.parse(template);
+  rendered = Mustache.render(template, {user: user});
+  $('#targetUserInfos').html(rendered);
+}
+
+function callbackRootChildren(error, children) {
+  if (error) {
+    throwError("Cannot fetch documents -> " + error);
+    throw error;
+  }
+  console.log('Root document has ' + children.entries.length + ' children');
+  var template = $('#children').html();
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {children: children.entries});
+  $('#targetChildren').html(rendered);
+}
+
+function callbackQuery(error, result) {
+  if (error) {
+    throwError("Cannot run query -> " + error);
+    throw error;
+  }
+  console.log('Root document has ' + result.entries.length + ' children');
+  var template = $('#children').html();
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {children: result.entries});
+  $('#targetChildren').html(rendered);
+}
+
+function callbackFetchDocument(error, doc) {
+  if (error) {
+    throwError("Cannot display document metadata -> " + error);
+    throw error;
+  }
+  RestAPI.currentDocument = doc;
+  if (doc.contextParameters.acls != undefined) {
+    var ace = doc.contextParameters.acls[0].ace;
+  }
+  console.log('Selecting ' + doc.title);
+  var template = $('#displayMetadata').html();
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {doc: doc, ace: ace});
+  $('#displayForm').html(rendered);
+  template = $('#editionMetadata').html();
+  Mustache.parse(template);
+  rendered = Mustache.render(template, {doc: doc});
+  $('#editionForm').html(rendered);
+}
+
+function callbackUpdateDocument(error, doc) {
+  if (error) {
+    throwError("Cannot display document metadata -> " + error);
+    throw error;
+  }
+  RestAPI.currentDocument = doc;
+  if (doc.contextParameters.acls != undefined) {
+    var ace = doc.contextParameters.acls[0].ace;
+  }
+  console.log('Selecting ' + doc.title);
+  var template = $('#displayMetadata').html();
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {doc: doc, ace: ace});
+  $('#displayForm').html(rendered);
+  template = $('#editionMetadata').html();
+  Mustache.parse(template);
+  rendered = Mustache.render(template, {doc: doc});
+  $('#editionForm').html(rendered);
+  toggle();
+}
+
+// Show modal for error
+function throwError(error) {
+  $('#errorValue').append(error);
+  $('#modalError').modal('show');
+}
+
+// Show modal confirm of deletion
+function displayDelete() {
+  $('#modalDelete').modal('show');
+}
+
+// Show modal for attaching blob
+function displayBlob() {
+  $('#modalBlob').modal('show');
+}
+
+// Toggle edit form
+function toggle() {
+  $('#editionForm').toggle();
+  $('#displayForm').toggle();
+}
+
+$(document)
+  .ready(function () {
+    //Build Nuxeo Client
+    RestAPI.client = RestAPI.config();
+
+    // Call and display default domain children
+    RestAPI.getRootChildren();
+
+    // Call and display current user
+    RestAPI.getCurrentUser();
+
+    // Call query when clicking on search
+    $('#search').click(function () {
+      RestAPI.executeQuery($('#searchInput').val());
+    });
+
+    $('#importAction').click(function () {
+      $('#modalImport').modal('show');
+    });
+
+    $('#importFiles').click(function () {
+      console.log("Files size: " + $('#files')[0].files.length);
+      importFiles($('#files')[0].files);
+    });
+
+    $('attachBlob').click(function () {
+      $('#blobModal').modal('show');
+    })
+
+    $('#importBlob').click(function () {
+      RestAPI.attachBlob($('#blob')[0].files[0]);
+    });
+
+    $('#refresh').click(function () {
+      location.reload();
+    });
+
+    $('#deleteDoc').click(function () {
+      RestAPI.deleteDocument();
+    });
+
+    $('#create').click(function () {
+      $('#modalCreate').modal('show');
+    });
+
+    $('#createDoc').click(function () {
+      var map = new Object();
+      $("#createDocForm :input").each(function () {
+        var name = $(this).attr('name');
+        var input = $(this).val();
+        map[name] = input;
+      });
+      RestAPI.createDocument(map);
+    });
+
+  }
+)
+;
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// DO NOT EDIT THE CODE ABOVE //////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
